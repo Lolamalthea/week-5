d63736628e95d99131cf92fdbd5663b977ca7ac9
Laurence Crevel <laurence.crevel@gmail.com>
2016-06-27 20:15:11 +0200

    First commit.

--- app/models/curiosity.rb
+++ app/models/curiosity.rb
@@ -1,2 +1,4 @@
 class Curiosity < ActiveRecord::Base
+  validates :category, inclusion: { in: %w("Penderie" "Coffre à jouets" "Vaisselle" "Décorations" "Amis" "Bibliothèque"),
+    message: "%{value} is not a valid size" }
 end
