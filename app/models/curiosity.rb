c525ffcfb941c881167753b9d203bf7c5ce29f51
Laurence Crevel <laurence.crevel@gmail.com>
2016-06-27 20:47:17 +0200

    Add validation to category.

--- app/models/curiosity.rb
+++ app/models/curiosity.rb
@@ -1,4 +1,4 @@
 class Curiosity < ActiveRecord::Base
-  validates :category, inclusion: { in: %w("Penderie" "Coffre à jouets" "Vaisselle" "Décorations" "Amis" "Bibliothèque"),
-    message: "%{value} is not a valid size" }
+  validates :category, inclusion: { in: ["Penderie", "Coffre à jouets", "Vaisselle", "Décorations", "Amis", "Bibliothèque"],
+    message: "%{value} is not a valid category" }
 end
